63b56c5f53ef6969ddc859ba0dabf067f767bbe3
Shadow <shadow10online@gmail.com>
2019-05-30 10:07:30 +0800

    [FocusMedia][DEMO] 1.add system init screen

---
+++ app/src/main/java/com/wharfofwisdom/focusmediaplayer/ServiceViewModel.java
@@ -0,0 +1,9 @@
+package com.wharfofwisdom.focusmediaplayer;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ServiceViewModel extends ViewModel {
+    MutableLiveData<String> status = new MutableLiveData<>();
+
+}
